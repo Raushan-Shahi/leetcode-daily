7771ac8b6a58c768258ec621fc9cb39bbed9479d
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-03-05 10:28:08 +0530

    day 37

---
+++ day37.cpp
@@ -0,0 +1,65 @@
+// Given an array of integers arr, you are initially positioned at the first index of the array.
+
+// In one step you can jump from index i to index:
+
+// i + 1 where: i + 1 < arr.length.
+// i - 1 where: i - 1 >= 0.
+// j where: arr[i] == arr[j] and i != j.
+// Return the minimum number of steps to reach the last index of the array.
+
+// Notice that you can not jump outside of the array at any time.
+
+
+
+
+
+
+
+class Solution {
+public:
+    int minJumps(vector<int>& arr) 
+    {
+        int n = arr.size();
+        unordered_map<int, vector<int>>mp;
+        for (int i = 0; i < n; i++) mp[arr[i]].push_back(i);
+        
+        queue<int>q;
+        vector<bool>visited(n, false);
+        q.push(0);
+        int steps = 0;
+        while(!q.empty())
+        {
+            int size = q.size();
+            while(size--)
+            {
+                int currIdx = q.front();
+                q.pop();
+                if (currIdx == n - 1) return steps;
+                //================================================================
+                //EXPLORE ALL POSSIBLE OPTIONS
+                if (currIdx + 1 < n && !visited[currIdx + 1])  //OPTION-1 (Move Back)
+                {
+                    visited[currIdx + 1] = true;
+                    q.push(currIdx + 1);
+                }
+                if (currIdx - 1 >= 0 && !visited[currIdx - 1]) //OPTION-2 (Move Forward)
+                {
+                    visited[currIdx - 1] = true;
+                    q.push(currIdx - 1);
+                }
+                for (int newIdx : mp[arr[currIdx]])  //OPTION-3 (Move to same valued idx)
+                {                                 //newIdx coud be before currIdx or after currIdx
+                    if (!visited[newIdx]) 
+                    {
+                        visited[newIdx] = true;
+                        q.push(newIdx);
+                    }
+                }
+                //===================================================================
+                mp[arr[currIdx]].clear();    //EXPLAINED BELOW :)
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
